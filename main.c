aae6142630fd2c0ea2948d3c30f47d8568e903af
adel051700 <s235076@dtu.dk>
2024-09-11 13:45:22 +0200

    made function to greyscale an image to save memory

--- main.c
+++ main.c
@@ -8,50 +8,46 @@
 #include <stdio.h>
 #include "cbmp.h"
 
-//Function to invert pixels of an image (negative)
-void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
-  for (int x = 0; x < BMP_WIDTH; x++)
-  {
-    for (int y = 0; y < BMP_HEIGTH; y++)
-    {
-      for (int c = 0; c < BMP_CHANNELS; c++)
-      {
-      output_image[x][y][c] = 255 - input_image[x][y][c];
-      }
+//Function to convert an image to greyscale to save memory
+void greyscale(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS],
+               unsigned char temp_image[BMP_WIDTH][BMP_HEIGTH]) {
+    for (int x = 0; x < BMP_WIDTH; x++) {
+        for (int y = 0; y < BMP_HEIGTH; y++) {
+            unsigned char grey = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) / 3;
+            temp_image[x][y] = grey;
+        }
     }
-  }
 }
 
-  //Declaring the array to store the image (unsigned char = unsigned 8 bit)
-  unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-  unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+//Declaring the array to store the image (unsigned char = unsigned 8 bit)
+unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char temp_image[BMP_WIDTH][BMP_HEIGTH];
+unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
 
 //Main function
-int main(int argc, char** argv)
-{
-  //argc counts how may arguments are passed
-  //argv[0] is a string with the name of the program
-  //argv[1] is the first command line argument (input image)
-  //argv[2] is the second command line argument (output image)
+int main(int argc, char **argv) {
+    //argc counts how may arguments are passed
+    //argv[0] is a string with the name of the program
+    //argv[1] is the first command line argument (input image)
+    //argv[2] is the second command line argument (output image)
 
-  //Checking that 2 arguments are passed
-  if (argc != 3)
-  {
-      fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
-      exit(1);
-  }
+    //Checking that 2 arguments are passed
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
+        exit(1);
+    }
 
-  printf("Example program - 02132 - A1\n");
+    printf("Example program - 02132 - A1\n");
 
-  //Load image from file
-  read_bitmap(argv[1], input_image);
+    //Load image from file
+    read_bitmap(argv[1], input_image);
 
-  //Run inversion
-  invert(input_image,output_image);
+    //Run inversion
+    greyscale(input_image, temp_image);
 
-  //Save image to file
-  write_bitmap(output_image, argv[2]);
+    //Save image to file
+    write_bitmap(output_image, argv[2]);
 
-  printf("Done!\n");
-  return 0;
+    printf("Done!\n");
+    return 0;
 }
