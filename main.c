f6f08e05f324ba982641a103d3001fadfca1d712
adel051700 <s235076@dtu.dk>
2024-09-11 14:17:00 +0200

    made function to make black and white

--- main.c
+++ main.c
@@ -4,24 +4,42 @@
 //To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
 //To run (win): main.exe example.bmp example_inv.bmp
 
-#include <stdlib.h>
-#include <stdio.h>
+
+
 #include "cbmp.h"
 
 //Function to convert an image to greyscale to save memory
 void greyscale(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS],
-               unsigned char temp_image[BMP_WIDTH][BMP_HEIGTH]) {
+               char temp_image[BMP_WIDTH][BMP_HEIGTH]) {
     for (int x = 0; x < BMP_WIDTH; x++) {
         for (int y = 0; y < BMP_HEIGTH; y++) {
-            unsigned char grey = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) / 3;
+            char grey = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) / 3;
             temp_image[x][y] = grey;
+        }
+    }
+}
+
+void black_and_white(char inputImage[BMP_WIDTH][BMP_HEIGTH],
+                     unsigned char outputImage[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]) {
+    for (int x = 0; x < BMP_WIDTH; x++) {
+        for (int y = 0; y < BMP_HEIGTH; y++) {
+            int grayscale = 0;
+            for (int c = 0; c < BMP_CHANNELS; c++) {
+                grayscale += inputImage[x][y];
+            }
+            grayscale /= BMP_CHANNELS;
+
+            unsigned char bw = (grayscale > 90) ? 255 : 0;
+            for (int c = 0; c < BMP_CHANNELS; c++) {
+                outputImage[x][y][c] = bw;
+            }
         }
     }
 }
 
 //Declaring the array to store the image (unsigned char = unsigned 8 bit)
 unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-unsigned char temp_image[BMP_WIDTH][BMP_HEIGTH];
+char temp_image[BMP_WIDTH][BMP_HEIGTH];
 unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
 
 //Main function
@@ -44,6 +62,7 @@ int main(int argc, char **argv) {
 
     //Run inversion
     greyscale(input_image, temp_image);
+    black_and_white(temp_image, output_image);
 
     //Save image to file
     write_bitmap(output_image, argv[2]);
